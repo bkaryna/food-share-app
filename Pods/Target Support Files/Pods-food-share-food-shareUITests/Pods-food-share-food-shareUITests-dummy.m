3af9675db9f227033742e1af5c75884ca4e34439
Karyna <karina.babenko16@gmail.com>
2021-10-29 20:03:01 +0200

    Install Firebase pods

---
+++ Pods/Target Support Files/Pods-food-share-food-shareUITests/Pods-food-share-food-shareUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_food_share_food_shareUITests : NSObject
+@end
+@implementation PodsDummy_Pods_food_share_food_shareUITests
+@end
---
+++ Pods/Target Support Files/Pods-food-shareTests/Pods-food-shareTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_food_shareTests : NSObject
+@end
+@implementation PodsDummy_Pods_food_shareTests
+@end
