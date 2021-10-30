8d5b586ce20d27ae28c30c31926122955bd2b8bd
Karyna <karina.babenko16@gmail.com>
2021-10-30 14:30:27 +0200

    Implement Sign up feature

---
+++ food-share/Helper/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  food-share
+//
+//  Created by Karyna Babenko on 29/10/2021.
+//
+
+import Foundation
+
+struct Constants {
+    struct Storyboard {
+        static let homeViewController = "HomeVC"
+    }
+}
