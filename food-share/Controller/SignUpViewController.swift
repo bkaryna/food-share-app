5cf1be6d70e8354b10da208d2bc567ac03af67b5
Karyna <karina.babenko16@gmail.com>
2021-10-30 14:30:27 +0200

    Implement Sign up feature

--- food-share/Controller/SignUpViewController.swift
+++ food-share/Controller/SignUpViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class SignUpViewController: UIViewController {
 
@@ -17,7 +19,58 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var SignUpButton: UIButton!
     @IBOutlet weak var ErrorLabel: UILabel!
     
+    
+    //check the fields and validate the data; return nil or error message
+    func validateFields() -> String? {
+        //check if all fields are filled in
+        if FirstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || LastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            EmailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            PasswordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all he required data"
+        }
+        
+        //check password security
+        let cleanedPassword = PasswordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if Validation.passwordValid(cleanedPassword) == false {
+            return "Please make sure your password meets the requirements"
+        }
+        
+        return nil
+    }
+    
+    
+    
     @IBAction func SignUpTapped(_ sender: Any) {
+//        fields validation
+//        let error = validateFields()
+//
+//        if error != nil{
+//            showError(error!)
+//        }
+        
+        let firstName = FirstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let lastName = LastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let email = EmailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = PasswordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        //user creation
+        Auth.auth().createUser(withEmail: email, password: password) { result, err in
+            if err != nil {
+                self.showError("Error creating user")
+            } else {
+                let db = Firestore.firestore()
+                db.collection("Users").document(result!.user.uid).setData([ "FirstName": firstName, "LastName": lastName ]) {(error) in
+                    if error != nil {
+                    // Show error message
+                        self.showError("Error saving user data")
+                    }
+                }
+                
+                // Transition to the home screen
+                self.transitionToHome()
+            }
+        }
+        
+        //go to home screen
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,5 +88,18 @@ class SignUpViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func showError(_ error: String) {
+        ErrorLabel.text=error;
+        ErrorLabel.alpha=1
+    }
+    
+    func transitionToHome() {
+        //storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController)
+        let homeViewController = storyboard?.instantiateViewController(identifier: "HomeVC") as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+    }
 
 }
