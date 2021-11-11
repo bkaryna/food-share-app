b1c4623187a889d5abf632735ef3861f49bba588
bkaryna <karyna.babenko@yahoo.com>
2021-11-11 23:12:09 +0100

    Add Home Screen navigation

---
+++ food-share/Controller/HomeTabsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTabViewController.swift
+//  food-share
+//
+//  Created by Karyna Babenko on 11/11/2021.
+//
+
+import UIKit
+
+class HomeTabsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
