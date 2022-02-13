4905648da10faec685fcd1d176ca0ad53c9d0df0
bkaryna <karyna.babenko@yahoo.com>
2022-02-13 21:45:22 +0100

    Add a dropdown list and populate it with options

--- food-share/Controller/OtherUsersItemsViewController.swift
+++ food-share/Controller/OtherUsersItemsViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import Lottie
+import DropDown
 
 class OtherUsersItemsViewController: UIViewController, UISearchResultsUpdating  {
     private let db = Firestore.firestore()
@@ -20,6 +21,12 @@ class OtherUsersItemsViewController: UIViewController, UISearchResultsUpdating  
     private var filterCondition: NSRegularExpression = NSRegularExpression(".*")
     private var tappedItem: UserItem!
     
+    let menu: DropDown = {
+        let menu = DropDown()
+        menu.dataSource = ["Price low to high", "Price high to low", "Newest first", "Close to me"]
+        return menu
+    }()
+    
     override func viewWillAppear(_ animated: Bool) {
         searchController.searchResultsUpdater = self
         navigationItem.searchController = searchController
@@ -28,6 +35,30 @@ class OtherUsersItemsViewController: UIViewController, UISearchResultsUpdating  
     override func viewDidLoad() {
         super.viewDidLoad()
         searchController.searchBar.becomeFirstResponder()
+        
+        let menuView = UIView(frame: navigationController?.navigationBar.frame ?? .zero)
+        navigationController?.navigationBar.topItem?.titleView = menuView
+        
+        guard let topMenuView = navigationController?.navigationBar.topItem?.titleView else {
+            return
+        }
+        
+        //menu.width = view.frame.width*0.5
+        menu.anchorView = topMenuView
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(topMenuBarTapped))
+        gesture.numberOfTapsRequired = 1
+        gesture.numberOfTouchesRequired = 1
+        topMenuView.addGestureRecognizer(gesture)
+        
+        menu.selectionAction = { index, title in
+            if (index == 2) {
+                self.otherUsersItemsList.sorted{
+                    $0.getValidFromDate()<$1.getValidFromDate()
+                }
+                self.otherItemsCollectionView.reloadData()
+            }
+        }
         //setUpAnimation
         CustomAnimation.setUp(view: view, animationView: animationView, frequency: 2, type: "loading")
         
@@ -40,6 +71,10 @@ class OtherUsersItemsViewController: UIViewController, UISearchResultsUpdating  
             self.animationView.isHidden = true
         }
         self.otherItemsCollectionView.reloadData()
+    }
+    
+    @objc func topMenuBarTapped(){
+        menu.show()
     }
     
     func updateSearchResults(for searchController: UISearchController) {
